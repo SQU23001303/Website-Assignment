fbd5ecd7c294f63ceddd47519e4ee1ab68657ac9
SQU23001303 <157488287+SQU23001303@users.noreply.github.com>
2024-04-15 23:14:29 +0100

    Add files via upload
    The developer has optimised a few bits of my website and added comments throughout so other users can understand

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function toggleDarkMode() {
+
+//Darkmode toggle
+function myFunction() {
   document.body.classList.toggle("dark-mode");
   localStorage.setItem("darkModeEnabled", document.body.classList.contains("dark-mode"));
 }
 
+//Gets the local dark mode storage
 window.onload = function() {
   var darkModeEnabled = localStorage.getItem("darkModeEnabled");
   if (darkModeEnabled === "true") {
     // If dark mode is enabled, toggle it on
-    toggleDarkMode();
+    myfunction();
   }
 };
 
-function myFunction(){
-  // Toggle dark mode for the body element
-  toggleDarkMode();
-};
+//Submit button 
+document.getElementById("submitButton").addEventListener("click", function() {
+  submit();
+});
+
+function submit() {
+  alert("Contact information submitted successfully");
+}
 
 
 
